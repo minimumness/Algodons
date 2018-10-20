ed62ed032ee1532634b2f80613c63696d4cf8c1f
minimumness <ganhuanmin@gmail.com>
2018-10-20 16:45:54 +0100

    Convert times to epoch function.

--- bs4.py
+++ bs4.py
@@ -1,15 +1,21 @@
 import bs4  # BeautifulSoup
 import requests
+from datetime import datetime
 
 
-def get_article_body(URL):
+def get_reuters_article_body(URL):
     
     page = requests.get(URL)
     soup = bs4.BeautifulSoup(page.text)
-    
     element = soup.select('div.StandardArticleBody_body')
     body = element[0].get_text()
-    
     return body
+
+def convert_date_to_epoch(time):
+    time = time[:17]
+    obj = datetime.strptime(time, '%Y%m%d %I:%M %p')
+    epoch = obj.timestamp()
+    return epoch
     
-a = get_article_body('http://www.reuters.com/article/entertainmentNews/idUSN2820662920070102')
+a = get_reuters_article_body('http://www.reuters.com/article/entertainmentNews/idUSN2820662920070102')
+b = convert_date_to_epoch('20070103 11:57 PM EST')
