8c49365ffea7fc98bbcb6e015efac3056f25732d
minimumness <ganhuanmin@gmail.com>
2018-10-20 14:50:39 +0100

    Function for returning the textbody from Reuters articles

--- bs4.py
+++ bs4.py
@@ -1,12 +1,15 @@
 import bs4  # BeautifulSoup
 import requests
 
-results = []
 
-page = requests.get('http://www.reuters.com/article/entertainmentNews/idUSN2820662920070102')
-soup = bs4.BeautifulSoup(page.text)
-
-element = soup.select('div.StandardArticleBody_body')
-movie = element[0].get_text()
-
-results.append(movie)
+def get_article_body(URL):
+    
+    page = requests.get(URL)
+    soup = bs4.BeautifulSoup(page.text)
+    
+    element = soup.select('div.StandardArticleBody_body')
+    body = element[0].get_text()
+    
+    return body
+    
+a = get_article_body('http://www.reuters.com/article/entertainmentNews/idUSN2820662920070102')
